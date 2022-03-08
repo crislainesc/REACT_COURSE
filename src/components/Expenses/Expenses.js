bbe756ec5547f385df23cdaaab88c6787035d002
Crislâine Santos <criis.santos@outlook.com>
2022-03-08 15:43:21 -0300

    feat: task-3 -> working with lists

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,18 +5,22 @@ import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020')
+  const [filteredYear, setFilteredYear] = useState('2022')
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear)
   };
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear
+  });
 
   return (
     <div>
 
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {props.items.map((expense) => (
+        {filteredExpenses.map((expense) => (
           <ExpenseItem
             key={expense.id}
             title={expense.title}
