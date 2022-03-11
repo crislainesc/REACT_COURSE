8e2b42d75a0987705ef5f474de2c752c6e1149e5
Crislâine Santos <criis.santos@outlook.com>
2022-03-11 15:01:09 -0300

    feat: add modal component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Modal from "../UI/Modal";
 
 import classes from './Cart.module.css'
 
@@ -15,7 +16,7 @@ const Cart = (props) => {
    })}</ul>;
 
    return (
-      <div>
+      <Modal>
          {cartItems}
          <div className={classes.total}>
             <span>Total Amount</span>
@@ -25,7 +26,7 @@ const Cart = (props) => {
             <button className={classes['button--alt']}>Close</button>
             <button className={classes.button}>Order</button>
          </div>
-      </div>
+      </Modal>
    );
 };
 
