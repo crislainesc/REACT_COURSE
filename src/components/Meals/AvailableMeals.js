506d020e52be235104b3addf7e9ff35d5974ba1a
Crislâine Santos <criis.santos@outlook.com>
2022-03-11 14:06:17 -0300

    feat: add meals component

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+import classes from './AvailableMeals.module.css'
+
+const DUMMY_MEALS = [
+   {
+      id: 'm1',
+      name: 'Sushi',
+      description: 'Finest fish and veggies',
+      price: 22.99,
+   },
+   {
+      id: 'm2',
+      name: 'Schnitzel',
+      description: 'A german specialty!',
+      price: 16.5,
+   },
+   {
+      id: 'm3',
+      name: 'Barbecue Burger',
+      description: 'American, raw, meaty',
+      price: 12.99,
+   },
+   {
+      id: 'm4',
+      name: 'Green Bowl',
+      description: 'Healthy...and green...',
+      price: 18.99,
+   },
+];
+
+
+const AvailableMeals = () => {
+
+   const mealsList = DUMMY_MEALS.map((meal) => {
+      return (
+         <li key={meal.id}>
+            <h2>{meal.name}</h2>
+            <p>{meal.description}</p>
+            <span>{meal.price}</span>
+         </li>
+      )
+   })
+
+   return (
+      <section className={classes.meals}>
+         <ul>
+            {mealsList}
+         </ul>
+      </section>
+   );
+};
+
+export default AvailableMeals;
