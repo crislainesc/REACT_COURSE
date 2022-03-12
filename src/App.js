5134ede501271f5a4309f27b1b3ed0140b29fe4f
Crislâine Santos <criis.santos@outlook.com>
2022-03-12 16:55:25 -0300

    fix: adjusting the custom hook logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
@@ -7,26 +7,26 @@ import useHttp from './hooks/use-http';
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = tasksObj => {
-    const loadedTasks = [];
-
-    for (const taskKey in tasksObj) {
-      loadedTasks.push({
-        id: taskKey,
-        text: tasksObj[taskKey].text
-      })
-    }
-
-    console.log(loadedTasks)
-  }
-
-  const httpData = useHttp('https://react-http-75081-default-rtdb.firebaseio.com/tasks.json', transformTasks);
-
-  const {isLoading, error, sendRequest: fetchTasks} = httpData
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+  
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({
+          id: taskKey,
+          text: tasksObj[taskKey].text
+        })
+      }
+  
+      setTasks(loadedTasks)
+    }
+
+    fetchTasks(
+      { url: 'https://react-http-75081-default-rtdb.firebaseio.com/tasks.json' },
+      transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
