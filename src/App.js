fd4911a20ce962e864a45cb79204c3a58eb2c2f7
Crislaine Santos <crislaine.costa@arapiraca.ufal.br>
2022-03-14 20:16:35 -0300

    feat: handling http states & feedback with redux

--- src/App.js
+++ src/App.js
@@ -1,60 +1,87 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { Fragment, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { uiActions } from './store/ui-slice';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { uiActions } from "./store/ui-slice";
+
+let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
-  const showCart = useSelector(state => state.ui.cartIsVisible);
-  const cart = useSelector(state => state.cart)
-  const notification = useSelector()
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
     const sendCartData = async () => {
-      dispatch(uiActions.showNotification({
-        state: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!'
-      }))
+      dispatch(
+        uiActions.showNotification({
+          state: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
 
-      const response = await fetch('https://react-http-75081-default-rtdb.firebaseio.com/cart.json', {
-        method: 'PUT',
-        body: JSON.stringify(cart)
-      })
+      const response = await fetch(
+        "https://react-http-75081-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
 
       if (!response.ok) {
-        dispatch(uiActions.showNotification({
-          state: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!'
-        }))
+        dispatch(
+          uiActions.showNotification({
+            state: "error",
+            title: "Error!",
+            message: "Sending cart data failed!",
+          })
+        );
       }
 
-      dispatch(uiActions.showNotification({
-        state: 'success',
-        title: 'Success!',
-        message: 'Sent cart data successfully!'
-      }))
+      dispatch(
+        uiActions.showNotification({
+          state: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
 
+    if(isInitial) {
+      isInitial = false;
+      return;
     }
 
-    sendCartData().catch(error => {
-      dispatch(uiActions.showNotification({
-        state: 'error',
-        title: 'Error!',
-        message: 'Sending cart data failed!'
-      }))
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          state: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
     });
-  }, [cart, dispatch])
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.state}
+          message={notification.message}
+          title={notification.title}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -10,7 +10,7 @@ const uiSlice = createSlice({
 
       showNotification(state, action) {
          state.notification = {
-            state: action.payload.status,
+            state: action.payload.state,
             title: action.payload.title,
             message: action.payload.message
          }
