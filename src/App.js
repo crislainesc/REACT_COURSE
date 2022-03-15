1b6c72752eb8756ccf64157b83e05081b24da509
Crislâine Santos <criis.santos@outlook.com>
2022-03-15 12:25:46 -0300

    feat: adding a layout wrapper component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { Route, Routes, Navigate } from 'react-router-dom'
+import Layout from './components/layout/Layout';
 import AllQuotes from "./pages/AllQuotes";
 import NewQuote from './pages/NewQuote';
 import QuoteDetail from './pages/QuoteDetail';
 
 function App() {
   return (
+    <Layout>
       <Routes>
         <Route path="/" exact element={<Navigate to='/quotes' />} />
         <Route path="/quotes/" exact element={<AllQuotes />} />
         <Route path="/quotes/:quoteId/*" element={<QuoteDetail />} />
         <Route path="/new-quote" element={<NewQuote />} />
       </Routes>
+    </Layout>
   );
 }
 
