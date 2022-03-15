744cbd3ade378aaefe2e05cfcd2bf4cb31815245
Crislâine Santos <criis.santos@outlook.com>
2022-03-15 13:52:04 -0300

    feat: implementing programmatic (imperative) navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,13 @@
+import { useNavigate } from "react-router";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+   const navigate = useNavigate();
+
    const addQuoteHandler = quoteData => {
       console.log(quoteData)
+
+      navigate('/quotes');
    }
 
    return (
