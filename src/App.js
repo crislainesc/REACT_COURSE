a76a8c2dd985ca192d4bd65aac4be6b4f33a55c8
Crislâine Santos <criis.santos@outlook.com>
2022-03-15 10:16:05 -0300

    feat: defining & using routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import { Route, Routes } from 'react-router-dom'
+import Products from './pages/Products';
+import Welcome from './pages/Welcome';
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Routes>
+        <Route path='/' element={<p>Hello Woooorld!</p>} />
+        <Route path='/welcome' element={<Welcome />} />
+        <Route path='/products' element={<Products />}/>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+// our-domani.com/ => Component A
+// our-domain.com/product => Component B
+
+
+// our-domani.com/ => Component A
+// our-domain.com/product => Component B
+
+
+// Versão Antiga da Lib:
+
+// function App() {
+//   return (
+//     <div>
+//       <Route path='/welcome'>
+//         <Welcome />
+//       </Route>
+//       <Route path='/products'>
+//         <Products />
+//       </Route>
+//     </div>
+//   );
+// }
