b87b87b56e381fcaca0a30c83d74787f9123bffa
Crislâine Santos <criis.santos@outlook.com>
2022-03-16 15:42:33 -0300

    feat: adding the comments features

--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,20 +1,43 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 import classes from './NewCommentForm.module.css';
 
+import LoadingSpinner from '../UI/LoadingSpinner';
+import { addComment } from '../../lib/api';
+import useHttp from '../../hooks/use-http';
+
 const NewCommentForm = (props) => {
+
+  const { sendRequest, status, error } = useHttp(addComment);
+
+  const { onAddedComment } = props;
+
   const commentTextRef = useRef();
+
+  useEffect(() => {
+    if (status === 'completed' && !error) {
+      onAddedComment();
+    }
+  }, [status, error, onAddedComment])
+
 
   const submitFormHandler = (event) => {
     event.preventDefault();
 
+    const enteredText = commentTextRef
+
     // optional: Could validate here
 
-    // send comment to server
+    sendRequest({ commentData: { text: enteredText.current.value }, quoteId: props.quoteId })
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === 'pending' && (
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      )}
       <div className={classes.control} onSubmit={submitFormHandler}>
         <label htmlFor='comment'>Your Comment</label>
         <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
