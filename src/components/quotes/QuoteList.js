b1b7011c4a82e96cc4a4e0ac58cd48b75d6da5f5
Crislâine Santos <criis.santos@outlook.com>
2022-03-16 09:32:21 -0300

    feat: working with query parameters

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,40 @@
 import { Fragment } from 'react';
+import { useNavigate, useLocation } from 'react-router';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = (props) => {
+  const navigate = useNavigate();
+  const locaction = useLocation();
+
+  const queryParams = new URLSearchParams(locaction.search);
+
+  const isSortingAscending = queryParams.get('sort') === 'asc';
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending)
+
+  const changeSortingHandler = () => {
+    navigate('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'))
+  }
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>Sort {isSortingAscending ? 'Ascending' : 'Descending'}</button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
