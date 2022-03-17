4045fca1efe7e3a2a012de8ddea19612ac6c44b9
Crislâine Santos <criis.santos@outlook.com>
2022-03-17 15:23:36 -0300

    feat: adding custom components & css modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+   return (
+      <MeetupDetail
+         image='https://psiueducacao.com/wp-content/uploads/2018/08/meetup-o-que-e-e-como-utilizar-na-sua-estrategia-de-comunicacao.jpeg'
+         title='Meetup title'
+         address='Some address meetup'
+         description='Meetup description'
+      />
+   );
+}
+
+export default MeetupDetails;
