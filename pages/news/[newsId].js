6009f1c92cabed4ee90db0946de19b390654f274
Crislâine Santos <criis.santos@outlook.com>
2022-03-17 14:00:06 -0300

    feat: extracting dynamic parameter values

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,7 +1,14 @@
-// our-domain.com/news/something-important
+import { useRouter } from 'next/router'
+
+// our-domain.com/news/newsId
 
 function DetailPage() {
-   return <h1>The Detail Page</h1>
- }
- 
- export default DetailPage;
+
+  const router = useRouter();
+
+  console.log(router.query.newsId)
+
+  return <h1>The Detail Page</h1>
+}
+
+export default DetailPage;
