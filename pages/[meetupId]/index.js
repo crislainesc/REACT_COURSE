df98d0c235619f1916eb6f122e1a30b7bcbeec14
Crislâine Santos <criis.santos@outlook.com>
2022-03-17 16:26:26 -0300

    feat: working with params for ssg data fetching

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -3,12 +3,31 @@ import MeetupDetail from "../../components/meetups/MeetupDetail";
 function MeetupDetails() {
    return (
       <MeetupDetail
-         image='https://psiueducacao.com/wp-content/uploads/2018/08/meetup-o-que-e-e-como-utilizar-na-sua-estrategia-de-comunicacao.jpeg'
-         title='Meetup title'
-         address='Some address meetup'
-         description='Meetup description'
+         image={meetupData.image}
+         title={meetupData.title}
+         address={meetupData.address}
+         description={meetupData.description}
       />
    );
+}
+
+export async function getStaticProps(context) {
+   // fetch data for a single meetup
+
+   const meetupId = context.params.meetupId
+   console.log(meetupId)
+
+   return {
+      props: {
+         meetupData: {
+            id: meetupId,
+            image: 'https://psiueducacao.com/wp-content/uploads/2018/08/meetup-o-que-e-e-como-utilizar-na-sua-estrategia-de-comunicacao.jpeg',
+            title: 'Meetup title',
+            address: 'Some address meetup',
+            description: 'Meetup description'
+         }
+      }
+   }
 }
 
 export default MeetupDetails;
