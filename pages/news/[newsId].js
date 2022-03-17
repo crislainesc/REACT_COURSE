7e31505982990cedadd9d34048390b066c345fb8
Crislâine Santos <criis.santos@outlook.com>
2022-03-17 14:11:14 -0300

    feat: linking between pages

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -6,7 +6,10 @@ function DetailPage() {
 
   const router = useRouter();
 
-  console.log(router.query.newsId)
+  const newsId = router.query.newsId;
+
+  // send a request to the backend api
+  // to fetch the news item with newsId
 
   return <h1>The Detail Page</h1>
 }
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,21 @@
 // our-domain.com/news
+import Link from 'next/link'
+import { Fragment } from "react";
 
 function NewsPage() {
-   return <h1>The News Page</h1>
- }
- 
- export default NewsPage;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/1">NextJS is a great Framework</Link>
+        </li>
+        <li>
+          <Link href="/news/2">Something Else</Link>
+        </li>
+      </ul>
+    </Fragment>
+  )
+}
+
+export default NewsPage;
