133b1a88bc13eeee0e3f0b779015cc1f72972be4
Crislaine Santos <crislaine.costa@arapiraca.ufal.br>
2022-03-17 20:20:31 -0300

    feat: sending http requests to our api routes

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,27 +1,25 @@
-import { MongoClient } from 'mongodb'
-import mongoUrl from '../../services/mongoConect'
+import { MongoClient } from "mongodb";
 
 // api/new-meetup
 // POST api/new-meetup
 
-function handler(req, res) {
-   if (req.method === 'POST') {
-      const data = req.body;
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
 
-      const client = await MongoClient.connect(mongoUrl);
-      const db = client.db();
+    const client = await MongoClient.connect('[MONGO BATABASE URL]');
+    const db = client.db();
 
-      const meetupsColletction = db.collection('meetups');
+    const meetupsColletction = db.collection("meetups");
 
-      const result = await meetupsColletction.insertOne(data);
+    const result = await meetupsColletction.insertOne(data);
 
-      console.log(result);
+    console.log(result);
 
-      client.close();
+    client.close();
 
-      res.status(201).json({message: 'Meetup inserted!'});
-
-   }
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
 }
 
 export default handler;
