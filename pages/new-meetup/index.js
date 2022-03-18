32f1726cc4cc9c374d791df0657468fe9886297f
Crislaine Santos <crislaine.costa@arapiraca.ufal.br>
2022-03-17 21:24:49 -0300

    feat: adding head metadata

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,9 +1,11 @@
 // our-domain.com/new-meetup
+import { Fragment } from "react";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-   const router = useRouter()
+  const router = useRouter();
 
   async function addMeetupHandler(enteredMeetupData) {
     const response = await fetch("/api/new-meetup", {
@@ -18,10 +20,21 @@ function NewMeetupPage() {
 
     console.log(data);
 
-    router.push('/')
+    router.push("/");
   }
 
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta
+          name="description"
+          content="Add your meetups and create amazing networking opportunites"
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Fragment>
+  );
 }
 
 export default NewMeetupPage;
