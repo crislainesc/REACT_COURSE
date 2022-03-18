8f328ac0b263821f27e174efd6f9522a1f6673fd
Crislâine Santos <criis.santos@outlook.com>
2022-03-18 17:08:21 -0300

    fix: grouping tests together with test suites

--- src/components/Greating.test.js
+++ src/components/Greating.test.js
@@ -1,15 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Greating from "./Greating";
 
-test('renders Hello World as a text', () => {
-   // Arrange
-   render(<Greating />)
+describe('Greating component', () => {
+   test('renders Hello World as a text', () => {
+      // Arrange
+      render(<Greating />)
 
-   // Act
+      // Act
 
-   // ... nothing
+      // ... nothing
 
-   // Assert
-   const helloWordlElement = screen.getByText('Hello World', { exact: false });
-   expect(helloWordlElement).toBeInTheDocument();
+      // Assert
+      const helloWordlElement = screen.getByText('Hello World', { exact: false });
+      expect(helloWordlElement).toBeInTheDocument();
+   });
 });
+
