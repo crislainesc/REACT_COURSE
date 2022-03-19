cddd3df9de55268fcde2e87327bae9c60da66186
Crislâine Santos <criis.santos@outlook.com>
2022-03-19 10:30:14 -0300

    feat: working with array & object types

--- basics.ts
+++ basics.ts
@@ -14,6 +14,27 @@ userName = "Max";
 
 let isInstructor: boolean;
 
+// More complex types
 
-let hobbies: null;
+let hobbies: string[];
 
+hobbies = ["Sports", "Cooking"];
+
+let person: {
+	name: string;
+	age: number;
+};
+
+person = {
+	name: "Max",
+	age: 12,
+};
+
+// person = {
+// 	isEmployee: true,
+// };
+
+let people: {
+	name: string;
+	age: number;
+}[];
