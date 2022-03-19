e543f9903914f1786bc1d31e7b86ae1333b7e429
Crislâine Santos <criis.santos@outlook.com>
2022-03-19 13:16:08 -0300

    feat: removing a todo

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -18,10 +18,17 @@ function App() {
 		});
 	};
 
+	const removeTodoHandler = (todoId: string) => {
+		
+		setTodos((prevTodos) => {
+			return prevTodos.filter(todo => todo.id !== todoId)
+		})
+	}
+
 	return (
 		<div>
 			<NewTodo onAddTodo={addTodoHandler} />
-			<Todos items={todos} />
+			<Todos items={todos} onRemoveTodo={removeTodoHandler} />
 		</div>
 	);
 }
