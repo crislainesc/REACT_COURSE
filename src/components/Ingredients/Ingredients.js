ca5a500e67861aa13a557b5802b8ae93a0d92ec3
Crislâine Santos <criis.santos@outlook.com>
2022-03-19 17:51:34 -0300

    feat: useEffect & loading data

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,6 +8,22 @@ function Ingredients() {
 
   const [userIngredients, setUserIngredients] = useState([]);
 
+  useEffect(() => {
+    fetch('https://react-http-75081-default-rtdb.firebaseio.com/ingredients.json').then(response => {
+      return response.json()
+    }).then(responseData => {
+      const loadedIngredients = []
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        })
+      }
+      setUserIngredients(loadedIngredients)
+    })
+  }, [])
+
   const addIngredientHandler = ingredient => {
     fetch('https://react-http-75081-default-rtdb.firebaseio.com/ingredients.json', {
       method: 'POST',
