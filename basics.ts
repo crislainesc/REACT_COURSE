43ecd9d217a2d8b753ab292f8333c64471a90ade
Crislâine Santos <criis.santos@outlook.com>
2022-03-19 10:25:28 -0300

    feat: exploring the base types

---
+++ basics.ts
@@ -0,0 +1,19 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number = 24;
+
+age = 12;
+
+let userName: string = "user";
+
+userName = "Max";
+
+let isInstructor: boolean;
+
+
+let hobbies: null;
+
--- with-typescript.ts
+++ with-typescript.ts
@@ -1,7 +1,9 @@
-function add(a: number, b: number) {
-  return a + b;
-}
+(function () {
+	function add(a: number, b: number) {
+		return a + b;
+	}
 
-const result = add(2, 5);
+	const result = add(2, 5);
 
-console.log(result);
+	console.log(result);
+})();
