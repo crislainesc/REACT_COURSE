7448b862d452caa93c9d299ec56df7c54b86f835
Crislâine Santos <criis.santos@outlook.com>
2022-03-20 14:24:06 -0300

    feat: adding links & navigation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from './pages/NewMeetup'
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from './components/layout/MainNavigation'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
   // my-page.com/
 
   return <div>
+    <MainNavigation/>
     <Switch>
       <Route path='/' exact={true}>
         <AllMeetupsPage />
