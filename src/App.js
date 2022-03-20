ba58e46415e8f25f394e034a41763978b486deb4
Crislâine Santos <criis.santos@outlook.com>
2022-03-20 14:59:16 -0300

    feat: building wrapper components with props.children

--- src/App.js
+++ src/App.js
@@ -3,27 +3,28 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from './pages/NewMeetup'
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from './components/layout/MainNavigation'
+import Layout from "./components/UI/Layout";
 
 function App() {
 
   // localhost:300
   // my-page.com/
 
-  return <div>
-    <MainNavigation/>
-    <Switch>
-      <Route path='/' exact={true}>
-        <AllMeetupsPage />
-      </Route>
-      <Route path='/new-meetup'>
-        <NewMeetupPage />
-      </Route>
-      <Route path='/favorites'>
-        <FavoritesPage />
-      </Route>
-    </Switch>
-  </div>;
+  return (
+    <Layout>
+      <Switch>
+        <Route path='/' exact={true}>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetupPage />
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </Layout>
+  );
 }
 
 export default App;
