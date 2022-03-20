fb2bdd2d30a407f5eb0434052e0d542461a3b7a9
Crislâine Santos <criis.santos@outlook.com>
2022-03-20 15:42:44 -0300

    feat: getting started with fetching data

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
-import DUMMY_DATA from '../data/dummy-data'
 
 const AllMeetupsPage = () => {
+
+   const [isLoading, setIsLoading] = useState(true)
+   const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+   fetch('https://react-http-75081-default-rtdb.firebaseio.com/meetups.json'
+   ).then(response => {
+      return response.json()
+   }).then(data => {
+      setIsLoading(false)
+      setLoadedMeetups(data)
+   })
+
+   if(isLoading) {
+      return <section>
+         <p>Loading...</p>
+      </section>
+   }
+
    return (
       <div>
          <h1>All Meetups</h1>
-         <MeetupList meetups={DUMMY_DATA} />
+         <MeetupList meetups={loadedMeetups} />
       </div>
    );
 };
